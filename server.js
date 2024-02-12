d24071a964b244e10022de5e4d4f6425782e5531
btadikon <btadikon@uncc.edu>
2024-02-12 10:32:25 -0500

    Added Angular

--- server.js
+++ server.js
@@ -2,29 +2,48 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const data = require("./data.json");
+const cors = require("cors");
 
-app.use('/', express.static('public'));
+app.use(cors());
 
-const budget = [
-    {
-      title: 'Eat out',
-      budget: 30
-    },
-    {
-      title: 'Rent',
-      budget: 300  
-    },
-    {
-      title: 'Groceries',
-      budget: 90  
-    },
-];
+const budget = {
+  myBudget: [
+  {
+    "title": "Eat out",
+    "budget": 30
+  },
+  {
+    "title": "Rent",
+    "budget": 300  
+  },
+  {
+    "title": "Groceries",
+    "budget": 90  
+  },
+  {
+      "title": "Insurance",
+      "budget": 50
+  },
+  {
+      "title": "Utilities",
+      "budget": 65
+  },
+  {
+      "title": "Electricity",
+      "budget": 120
+  },
+  {
+      "title": "others",
+      "budget": 50
+  }
+]
+};
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.send(data);
+    res.json(budget);
 });
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}');
